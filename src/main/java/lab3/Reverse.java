01be1082cba84c86b66165895da9681b4f04dde7
Serban <serbanhepcal@yahoo.com>
2023-10-23 18:54:35 +0300

    First commit

---
+++ src/main/java/lab3/Reverse.java
@@ -0,0 +1,17 @@
+package lab3;
+
+public class Reverse{
+    public static void main(String[] arg) {
+
+        String str = "Metoda", nstr="";
+        char ch;
+
+        System.out.println("Cuvantul inante de modificare " +str);
+        // System.out.println();
+        for(int i =0; i<str.length();i++){
+            ch = str.charAt(i);
+            nstr= ch+nstr;
+        }
+        System.out.println("String-ul reversed este " +nstr);
+    }
+}
